cdfd2546930b7634c6cbea4d0c7815e93b7ef7da
Max Yevtushok <maxshok15@gmail.com>
2022-09-18 17:57:58 +0300

    change1

---
+++ Unitylab1(1)/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+    [NonSerialized] public float rotationSpeed = 90f;
+    private float gravity = -20f;
+    public float jumpHeight = 15f;
+
+    public CharacterController characterController;
+    Vector3 movementVector;
+    Vector3 rotationVector;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+
+        if (characterController.isGrounded)
+        {
+            movementVector = transform.forward * movementSpeed * vInput;
+            rotationVector = transform.up * rotationSpeed * hInput;
+            if (Input.GetButtonDown("Jump"))
+            {
+                movementVector.y = jumpHeight;
+            }
+        }
+
+        movementVector.y += gravity * Time.deltaTime;
+        characterController.Move(movementVector * Time.deltaTime);
+        transform.Rotate(rotationVector * Time.deltaTime);
+    }
+}
